d123027c7327495283ded1c23c770051ceaccdcf
Stanislav Karlov <karlovvski@gmail.com>
2026-09-05 22:55:05 +0300

    feat: add simple moving average signal cleaning

--- src/main.c
+++ src/main.c
@@ -12,11 +12,15 @@
 #define ADC_BITWIDTH ADC_BITWIDTH_12
 
 #define LED_PIN 5
+#define MOVING_AVERAGE_N 5
 
-#define TIME_DIVIDER        1000
+#define TIME_DIVIDER 1000
+#define MOVING_AVERAGE_POLLING_MS 20
+#define OPERATIONAL_STEP_MS 100
 #define LOGGER_THRESHOLD_MS 500
 #define SENSOR_THRESHOLD_MV 2400
 
+uint64_t last_operation_at = 0;
 uint64_t last_log_at = 0;
 
 adc_oneshot_unit_handle_t adc_handle;
@@ -43,14 +47,33 @@ gpio_config_t gpio_config_led = {
     .intr_type = GPIO_INTR_DISABLE,
 };
 
-void handle_led(uint8_t pin)
+typedef struct
 {
+    float value_prev;
+    int values[MOVING_AVERAGE_N];
+    uint64_t updated_at;
+    uint16_t polling_ms;
+    uint8_t length;
+} moving_average_config;
 
-}
+moving_average_config mv_config = {
+    .value_prev = 0,
+    .values = {0},
+    .length = MOVING_AVERAGE_N,
+    .updated_at = 0,
+    .polling_ms = MOVING_AVERAGE_POLLING_MS,
+};
 
-void adc_filter(int vt_mv, int* cleaned_mv)
+void adc_sma_filter(moving_average_config* config) // simple moving average
 {
-    *cleaned_mv = vt_mv;
+    int sum = 0;
+
+    for (uint8_t i = 0; i < config->length; i++)
+    {
+        sum = sum + config->values[i];
+    }
+
+    config->value_prev = (float)sum / config->length;
 }
 
 void init()
@@ -66,32 +89,42 @@ void loop()
 {
     uint64_t now = esp_timer_get_time() / TIME_DIVIDER;
 
-    if ((now - last_log_at) > LOGGER_THRESHOLD_MS)
+    if ((now - mv_config.updated_at) >= MOVING_AVERAGE_POLLING_MS)
+    {
+        mv_config.updated_at = now;
+
+        for (uint8_t i = 0; i < mv_config.length; i++)
+        {
+            if (i == (mv_config.length - 1))
+            {
+                adc_oneshot_read(adc_handle, ADC_CHANNEL, &mv_config.values[i]); // pre-writing it raw
+                adc_cali_raw_to_voltage(adc_cali_handle, mv_config.values[i], &mv_config.values[i]);
+                break;
+            }
+            mv_config.values[i] = mv_config.values[i + 1];
+        }
+    }
+
+    if ((now - last_operation_at) >= OPERATIONAL_STEP_MS)
+    {
+        last_operation_at = now;
+
+        adc_sma_filter(&mv_config);
+        uint8_t led_level = mv_config.value_prev >= SENSOR_THRESHOLD_MV ? 0 : 1;
+        gpio_set_level(LED_PIN, led_level);
+    }
+
+    if ((now - last_log_at) >= LOGGER_THRESHOLD_MS)
     {
         last_log_at = now;
-
-        int raw = 0;
-        int voltage_calibrated_mv = 0;
-        int voltage_cleaned_mv = 0;
-
-        adc_oneshot_read(adc_handle, ADC_CHANNEL, &raw);
-        adc_cali_raw_to_voltage(adc_cali_handle, raw, &voltage_calibrated_mv);
-
-        // TODO: analog signal cleaning algorythm
-        adc_filter(voltage_calibrated_mv, &voltage_cleaned_mv);
-
-        uint8_t led_level = voltage_cleaned_mv >= SENSOR_THRESHOLD_MV ? 0 : 1;
-
-        gpio_set_level(LED_PIN, led_level);
-
-        ESP_LOGI("ADC", "calibrated (mv): %d; cleaned (mv): %d", voltage_calibrated_mv, voltage_cleaned_mv);
+        ESP_LOGI("ADC", "raw (mv): %d; cleaned (mv): %f", mv_config.values[MOVING_AVERAGE_N - 1], mv_config.value_prev);
     }
 }
 
 void app_main()
 {
     init();
-    
+
     while (1)
     {
         loop();
